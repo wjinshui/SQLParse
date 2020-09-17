9fe47ee04c7cabea38998d051cbeeeebad2eaf38
wjinshui <ymkscom@gmail.com>
2020-09-17 16:43:51 +0800

    initial

---
+++ Software/src/Demo.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		String name, pwd;
+		name = scanner.next();
+		pwd = scanner.next();
+		
+
+	}
+
+}
